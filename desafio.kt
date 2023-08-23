49cbd0ff745c57596d72fb8d01e2872510e47238
Giovanna092 <109235337+Giovanna092@users.noreply.github.com>
2023-08-22 21:00:09 -0300

    Minha resolucao do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,8 +1,12 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+class Usuario(val nome: String, val email: String, val dataInscricao: String) {
+    fun exibirInformacoes() {
+        println("Nome: $nome")
+        println("E-mail: $email")
+        println("Data de Inscrição: $dataInscricao")
+    }
+}
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
@@ -11,11 +15,42 @@ data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) 
     val inscritos = mutableListOf<Usuario>()
     
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        //adicionando usuario à lista de inscritos
+        inscritos.add(usuario)
+    }
+    
+    fun exibir() {
+        
+        println("\nConteudos da formacao $nome: ")
+        
+        //listando os conteudos educacionais da formacao
+		conteudos.forEach { conteudo ->
+        	println("${conteudo.nome} - Duração: ${conteudo.duracao} minutos")
+    	}
+        
+        println("\nInscritos na formacao $nome: ")
+        //listando os inscritos
+        inscritos.forEach { inscrito -> 
+        	inscrito.exibirInformacoes()
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+ 
+    val conteudo1 = ConteudoEducacional("Introdução ao Kotlin", 120)
+	val conteudo2 = ConteudoEducacional("Programação Orientada a Objetos em Kotlin", 180)
+	val conteudos = listOf(conteudo1, conteudo2)
+    
+    val usuario1 = Usuario("joao da Silva", "joaosilva@email.com.br", "22/08/2023")
+    val usuario2 = Usuario("Ana Clara", "anaclara@email.com.br", "12/08/2023")
+    val usuario3 = Usuario("Usuario 3", "user3@email.com.br", "15/08/2023")
+    
+    val fKotlin = Formacao("Formacao kotlin developer", conteudos)
+    
+    fKotlin.matricular(usuario1)
+    fKotlin.matricular(usuario2)
+    fKotlin.matricular(usuario3)
+    
+    fKotlin.exibir()
 }
